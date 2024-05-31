70144a19fe8db0ead045d4290fdd18557a73f5b6
kero <124936213+kerosenee@users.noreply.github.com>
2024-05-31 08:36:05 -0400

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,1 +1,14 @@
+import random
 
+date_list = ["sushi date", "paintball date", "stargazing"]
+
+input_date = input("enter date to add to list")
+
+date_list.append(input_date)
+
+for x in date_list:
+    print(x)
+
+random_date = random.choice(date_list)
+
+print(str(random_date))
